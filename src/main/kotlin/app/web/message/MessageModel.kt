bf1dbda014b84ada0b38e6c4c4b61aad25c1df9c
Paxexterminatus <Paxexterminatus@gmail.com>
2017-09-26 01:02:45 +0300

    css fix

--- src/main/kotlin/app/web/message/MessageModel.kt
+++ src/main/kotlin/app/web/message/MessageModel.kt
@@ -6,22 +6,23 @@ class MessageModel: BaseModel() {
 
     fun billCourse(queryString: String): MutableMap<String, Any> {
         var data: MutableMap<String, Any> = mutableMapOf()
-        data.plus(Pair("clientId",0))
-        data.plus(Pair("clientName",""))
-        data.plus(Pair("clientEmail",""))
-        data.plus(Pair("itemName",""))
-        data.plus(Pair("totalPrice",0f))
-        data.plus(Pair("userName",""))
+        data["clientId"] = 0
+        data["clientName"] = "client"
+        data["clientEmail"] = "paxexterminatus@gmail.com"
+        data["itemName"] = "item"
+        data["totalPrice"] = 0f
+        data["userName"] = "user"
 
-        var rs = callSas("billCourse",queryString)
-        while (rs.next()) {
-            data["clientId"] = rs.getInt("CLIENT_ID")
-            data["clientName"] = rs.getString("CLIENT_NAME").toLowerCase().capitalize()
-            data["clientEmail"] = rs.getString("CLIENT_EMAIL")
-            data["itemName"] = rs.getString("PRODUCT_NAME")
-            data["totalPrice"] = rs.getFloat("TOTAL_PRICE")
-            data["userName"] = rs.getString("USER_NAME")
-        }
+//        var rs = callSas("billCourse",queryString)
+//        while (rs.next()) {
+//            data["clientId"] = rs.getInt("CLIENT_ID")
+//            data["clientName"] = rs.getString("CLIENT_NAME").toLowerCase().capitalize()
+//            data["clientEmail"] = rs.getString("CLIENT_EMAIL")
+//            data["itemName"] = rs.getString("PRODUCT_NAME")
+//            data["totalPrice"] = rs.getFloat("TOTAL_PRICE")
+//            data["userName"] = rs.getString("USER_NAME")
+//        }
+
         return data
     }
 }
